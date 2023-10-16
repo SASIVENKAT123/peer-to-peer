2262104a18f4a28a4dbf0e833b989c133281bfdf
sasivenkat143143@outlook.com <sasivenkat143143@outlook.com>
2023-10-16 11:14:57 +0000

    Edited Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -15,3 +15,38 @@ class Form1(Form1Template):
 
 
     # Any code you write here will run before the form opens.
+
+  def primary_color_1_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    open_form("login")
+
+  def link_1_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    open_form("Borrow")
+
+  def link_2_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    open_form("Lend")
+
+  def link_3_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    open_form("FAQ")
+
+  def link_4_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    open_form("Feedback")
+
+  def link_5_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    open_form("Chat")
+
+  def primary_color_2_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    open_form("signup")
+
+
+
+
+
+
+
